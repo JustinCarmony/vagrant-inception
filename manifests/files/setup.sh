f551571067cf68ee0ac176477de2f776a7009547
Justin Carmony <justin@justincarmony.com>
2012-07-17 11:16:34 -0600

    Tweaks to inception

--- manifests/files/setup.sh
+++ manifests/files/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mkdir -p /var/vagrant
+git clone https://github.com/JustinCarmony/vagrant-inception.git /var/vagrant
 cd /var/vagrant
-
+vagrant box add lucid32 http://files.vagrantup.com/lucid32.box
+vagrant up
--- manifests/lucid32.pp
+++ manifests/lucid32.pp
@@ -1,3 +1,14 @@
 class inception {
-  exec { '
+  package { "git-core": ensure => present } 
+  exec { "gem install vagrant": 
+    unless => "which vagrant",
+    require => Package["git-core"]
+  }
+  exec { 'bash /vagrant-files/setup.sh &':
+    unless => "ls /var/vagrant/.vagrant",
+    command => "bash /vargrant-files/setup.sh &",
+    require => Exec["gem install vagrant"]
+  }
 }
+
+include inception
